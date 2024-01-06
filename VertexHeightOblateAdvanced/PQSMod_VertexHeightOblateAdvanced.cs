4031aa9632b2b83970c5c1489d717236e1847cd4
James Glaze <jamespglaze@gmail.com>
2024-01-06 15:29:34 -0600

    Add project files.

---
+++ VertexHeightOblateAdvanced/PQSMod_VertexHeightOblateAdvanced.cs
@@ -0,0 +1,134 @@
+﻿using Kopernicus.Components;
+using Kopernicus.Configuration;
+using Kopernicus.Constants;
+using System;
+using System.Collections.Generic;
+using Kopernicus.Configuration.Parsing;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace VertexHeightOblateAdvanced
+{
+    public class PQSMod_VertexHeightOblateAdvanced : PQSMod
+    {
+        public enum OblateModes
+        {
+            PointEquipotential,
+            Blend,
+            //Maclaurin,
+            //Jacobi,
+            CustomEllipsoid,
+        }
+
+        public OblateModes mode = OblateModes.PointEquipotential;
+        public double criticality = 0.0f;
+        //public double period = 3600f;
+        public double a = 1.0f;
+        public double b = 1.0f;
+        public double c = 1.0f;
+
+        private double aSqr = 1.0f;
+        private double bSqr = 1.0f;
+        private double cSqr = 1.0f;
+
+        private void PrecalculateValues()
+        {
+            // Clamp criticality to the interval [0,1]
+            if (criticality < 0.0f)
+            {
+                criticality = 0.0f;
+            }
+            if (criticality > 1.0f)
+            {
+                criticality = 1.0f;
+            }
+            // Precompute squares of each axis
+            aSqr = Math.Pow(a, 2);
+            bSqr = Math.Pow(b, 2);
+            cSqr = Math.Pow(c, 2);
+
+            /*switch (mode)
+            {
+
+                case OblateModes.PointEquipotential:
+                    break;
+                case OblateModes.Blend:
+                    break;
+                case OblateModes.CustomEllipsoid:
+                    break;
+                default:
+                    break; ;
+            }*/
+        }
+
+        private double CalculateDeformityPointEquipotential(double theta)
+        {
+            if (theta <= 0.0f || theta >= Math.PI * 1.0f || criticality == 0.0f)
+            {
+                return 1;
+            }
+            try
+            {
+                double heightScaleFactor = 3 * Math.Cos((Math.PI + Math.Acos(criticality * Math.Sin(theta))) / 3) / (criticality * Math.Sin(theta));
+                if (heightScaleFactor > 1.5f)
+                {
+                    return 1.5f;
+                }
+                if (heightScaleFactor < 1.0f)
+                {
+                    return 1;
+                }
+                return heightScaleFactor;
+            }
+            catch (Exception e)
+            {
+                return 1;
+            }
+        }
+
+        private double CalculateDeformityCustomEllipsoid(double phi, double theta)
+        {
+            try
+            {
+                double term1 = Math.Pow(Math.Sin(theta), 2) * Math.Pow(Math.Cos(phi), 2) / aSqr;
+                double term2 = Math.Pow(Math.Sin(theta), 2) * Math.Pow(Math.Sin(phi), 2) / bSqr;
+                double term3 = Math.Pow(Math.Cos(theta), 2) / cSqr;
+                return 1 / Math.Sqrt(term1 + term2 + term3);
+            }
+            catch (Exception e)
+            {
+                return 1;
+            }
+        }
+
+        private double CalculateDeformity(double vertHeight, double u, double v)
+        {
+            double phi = 2* Math.PI * u;
+            double theta = Math.PI * v;
+
+            switch (mode)
+            {
+                case OblateModes.PointEquipotential:
+                    return vertHeight * CalculateDeformityPointEquipotential(theta);
+                case OblateModes.Blend:
+                    return vertHeight * CalculateDeformityPointEquipotential(theta) * CalculateDeformityCustomEllipsoid(phi, theta);
+                case OblateModes.CustomEllipsoid:
+                    return vertHeight * CalculateDeformityCustomEllipsoid(phi, theta);
+                default:
+                    return vertHeight;
+            }
+        }
+
+        public override void OnSetup()
+        {
+            base.OnSetup();
+            PrecalculateValues();
+        }
+
+        public override void OnVertexBuildHeight(PQS.VertexBuildData data)
+        {
+            //Apply height
+            data.vertHeight = CalculateDeformity(data.vertHeight, data.u, data.v);
+        }
+    }
+}
---
+++ VertexHeightOblateAdvanced/VertexHeightOblateAdvanced.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Kopernicus.ConfigParser.Attributes;
+using Kopernicus.ConfigParser.BuiltinTypeParsers;
+using Kopernicus.Configuration.ModLoader;
+using Kopernicus.Configuration.Parsing;
+using Kopernicus.ConfigParser.Enumerations;
+using UnityEngine;
+using Kopernicus.Configuration.Enumerations;
+using LibNoise;
+using static VertexHeightOblateAdvanced.PQSMod_VertexHeightOblateAdvanced;
+
+namespace VertexHeightOblateAdvanced
+{
+    [RequireConfigType(ConfigType.Node)]
+    public class VertexHeightOblateAdvanced : ModLoader<PQSMod_VertexHeightOblateAdvanced>
+    {
+        // What oblate mode to use for this mod
+        // PointEquipotential: Follow gravitational equipotential of a rotating point mass
+        // Blend: PointEquipotential multiplied by CustomEllipsoid
+        // Maclaurin: 2-Axis oblate spheroid solution for gravitational equipotential of rotating body of uniform density
+        // Jacobi: 3-Axis ellipsoid solution for gravitational equipotential of rotating body of uniform density
+        // CustomEllipsoid: Generate ellipsoid from provided 3 axis a, b, c
+        [ParserTarget("mode")]
+        public EnumParser<OblateModes> Mode
+        {
+            get { return Mod.mode; }
+            set { Mod.mode = value.Value; }
+        }
+
+        // Angular velocity as a fraction of breakup angular velocity
+        [ParserTarget("criticality")]
+        public NumericParser<Double> Criticality
+        {
+            get { return Mod.criticality; }
+            set { Mod.criticality = value; }
+        }
+
+        // Rotational period of the body
+        /*[ParserTarget("period")]
+        public NumericParser<Double> Period
+        {
+            get { return Mod.period; }
+            set { Mod.period = value; }
+        }*/
+
+        // Primary equatorial axis as ratio of nominal body radius
+        [ParserTarget("a")]
+        public NumericParser<Double> A
+        {
+            get { return Mod.a; }
+            set { Mod.a = value; }
+        }
+
+        // Secondary equatorial axis as ratio of nominal body radius
+        [ParserTarget("b")]
+        public NumericParser<Double> B
+        {
+            get { return Mod.b; }
+            set { Mod.b = value; }
+        }
+
+        // Polar axis as ratio of nominal body radius
+        [ParserTarget("c")]
+        public NumericParser<Double> C
+        {
+            get { return Mod.c; }
+            set { Mod.c = value; }
+        }
+    }
+}
